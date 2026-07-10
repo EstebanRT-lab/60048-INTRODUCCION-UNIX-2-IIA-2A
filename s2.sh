a5f4061588d6c738a32ccad52294922d95395b56
EstebanRT-lab <estebanrojastobar@gmail.com>
2026-07-10 00:59:36 +0000

    Clase 2

---
+++ s2.sh
@@ -0,0 +1,18 @@
+# ssh me permite conectarme a ordenadores externos, por ejemplo con este código me conecto a bandit 0
+
+# Clear sirve para limpiar el historial 
+
+#history sirve para ver el historial 
+
+#pwd (print working directory) me muestra el directorio  en la que estoy trabajando en este momento 
+
+#cd (change directory) me sirve para cambiar de directorio
+
+#ls (list) me da una lista de comandos y directorios 
+#ls --all ó ls -a muestran TODOS los directorios, son un comando extendido y un abreviado 
+
+#los archivos y los directorios que empiezan con punto "." son ocultos
+# el punto "." simboliza el directorio actual 
+#El punto punto ".." simboliza a la carpeta madre o contenedora del directorio en el que estoy 
+
+#cd / nos lleva al directorio raiz, el que contiene a todos los otros directorios 
