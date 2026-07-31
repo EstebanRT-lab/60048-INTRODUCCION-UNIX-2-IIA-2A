abcbfffe20cdc47358ec6d512424bcf2f77bf7f0
EstebanRT-lab <estebanrojastobar@gmail.com>
2026-07-31 00:41:51 +0000

    Semana 4

---
+++ s4.sh
@@ -0,0 +1,14 @@
+#mkdir para crear directorios y despues se confirma con ls
+#touch podemos crear archivos
+#tanto con mkdir como con touch se pueden crear varios directorios/archivos a la ves, solo se tiene que enlistar todos separados por espacio
+#si ya existe un directorio con el mismo nombre, sale un mensaje de error (pero si había varios en la lista, los SI se crean)
+#rmdir sirve para eliminar un directorio
+#cp para copiar un archivo en un directorio utilizamos cp [nombrearchivo] [directorio/nuevonombredelarchivo]
+#mv para mover un archivo a un lugar distinto (misma estructura que en cp)
+#mv también nos sirve para renombrar un archivo, poniendo mv [nombredelarchivo] [nuevonombredelarchivo]
+#para instalar una aplicacion ponemos sudo apt y el nombre del archivo
+#pico [nombre del archivo] para agregar contendio dentro del archivo, dento "control" sirve para elegir las funciones de la app
+#si se escribe algo se puede confirmar que se haya guardado con cat
+#wc nos da detalles de cantidad de lineas palabras y caracteres en el archivo
+#wc + -l (muestra lineas), -w (palabras) y -c (caracteres)
+#tar + -cvf se usa para unir archivos, -cvfz para comprimir varios archivos, tar -xvf para descomprimir
